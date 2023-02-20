76e34856b40cee5eff7b6727ae5dd41da15449f5
Булат Габдрахманов <semichkin@MacBook-Pro-Bulat.local>
2023-02-20 23:06:50 +0100

    Shorted package name; Added funcs FromJSON and JSON; changed func Convert to Struct

---
+++ conv.go
@@ -0,0 +1,45 @@
+package conv
+
+import (
+	"encoding/json"
+	"github.com/mitchellh/mapstructure"
+	"github.com/semichkin-gopkg/conf"
+)
+
+func Struct[T any](
+	source any,
+	configurators ...conf.Updater[mapstructure.DecoderConfig],
+) (T, error) {
+	var target T
+
+	config := conf.NewBuilder[mapstructure.DecoderConfig]().
+		Fix(func(c *mapstructure.DecoderConfig) {
+			c.Result = &target
+		}).
+		Append(func(c *mapstructure.DecoderConfig) {
+			c.Metadata = nil
+			c.TagName = "json"
+		}).
+		Append(configurators...).
+		Build()
+
+	decoder, err := mapstructure.NewDecoder(&config)
+	if err != nil {
+		return target, err
+	}
+
+	if err = decoder.Decode(source); err != nil {
+		return target, err
+	}
+
+	return target, nil
+}
+
+func JSON(data any) ([]byte, error) {
+	return json.Marshal(data)
+}
+
+func FromJSON[T any](bytes []byte) (T, error) {
+	var data T
+	return data, json.Unmarshal(bytes, &data)
+}
--- go.mod
+++ go.mod
@@ -1,8 +1,8 @@
-module github.com/semichkin-gopkg/converter
+module github.com/semichkin-gopkg/conv
 
 go 1.19
 
 require (
 	github.com/mitchellh/mapstructure v1.5.0
-	github.com/semichkin-gopkg/configurator v0.0.1
+	github.com/semichkin-gopkg/conf v0.0.3
 )
